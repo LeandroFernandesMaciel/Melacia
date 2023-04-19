97277f4c3abb51e435c24883ab9e21bf061b065c
Abraão <abraao.l.silva6@aluno.senai.br>
2023-04-19 17:51:31 -0300

    v1

---
+++ VerduraoDoJoao.Melanciometro/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection.Emit;
+
+namespace VerduraoDoJoao.Melanciometro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Melancia Comum 2-Melancia Baby");
+            int melancia = int.Parse(Console.ReadLine());
+            double preco;
+            string resp;
+            double
+
+            do
+            {
+                if (melancia == 1)
+                {
+                    preco = 5.50;
+                    Console.WriteLine("Melancia Comum: " + preco.ToString());
+
+                }
+                else if (melancia == 2)
+                {
+                    preco = 8.56;
+                    Console.WriteLine("Melancia Baby " + preco.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Não existe essa opção!");
+                    melancia = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("1-Melancia Comum 2-Melancia Baby");
+                melancia = int.Parse(Console.ReadLine());
+
+
+
+            } while (melancia != 0);
+
+
+
+            ////int cont = 1;
+            ////while (cont < 10)
+            ////{
+            ////    Console.WriteLine("Você entendeu?");
+            ////    if (cont == 3)
+            ////        break;
+            ////    cont = cont + 1;
+            ////}
+
+
+
+        }
+    }
+
+
+
+
+        }
+
